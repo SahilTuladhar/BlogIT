f101d4e2fecd1d6442af162c4af9db1b26970a07
Sahil Tuladhar <sailratna11@gmail.com>
2024-10-08 12:30:51 +0545

    registration feature added

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
   path('' , views.blog , name = 'blog' ) , # '' - blank means home url
-  path('create-blog' , views.create_blog , name =  'create-blog')
+  path('create-blog' , views.create_blog , name =  'create-blog'),
+  path('register' , views.register , name = 'register'),
+  path('login' , views.login , name = 'login')
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.conf import settings
+from django.contrib.auth.models import User , auth
+from django.contrib import messages
 from .models import Blog
 
 # Create your views here.
@@ -31,4 +33,50 @@ def create_blog(request):
 
   else:
     return render(request , 'create.html' , {'MEDIA_URL' : settings.MEDIA_URL})
+  
+
+def register(request):
+ 
+ if request.method == 'POST':
+
+  Username = request.POST['username']
+  Email = request.POST['email']
+  Password = request.POST['password']
+  Re_password = request.POST['re-password']
+
+  if Password == Re_password:
+
+    if User.objects.filter(username = Username).exists():
+
+      messages.info(request , 'Username already Exists')
+      return redirect('register')
+    
+    elif User.objects.filter(email = Email).exists():
+
+      messages.info(request , 'Email already registered')
+      return redirect('register')
+    
+    else:
+
+      user = User.objects.create_user(username=Username , email=Email ,  password=Password)
+
+      user.save()
+      messages.info(request , "User Created Successfully")
+
+      return redirect('login')
+  
+  else:
+
+    messages.info(request , ' Password does not match')
+    return redirect('register')
+
+
+ else:
+
+  return render(request ,  'register.html' , {'MEDIA_URL' : settings.MEDIA_URL})
+
+def login(request):
+ 
+  return render(request ,   'login.html' , {'MEDIA_URL' : settings.MEDIA_URL})
+
  
